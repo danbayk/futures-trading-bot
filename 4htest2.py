b40825b9a702b44255e7e03349e83882cae8ebd5
danbayk <danboy2k1@gmail.com>
2023-04-30 21:09:08 -0400

    update strat

--- 4htest2.py
+++ 4htest2.py
@@ -3,6 +3,7 @@ from backtrader.feeds import GenericCSVData
 import pandas as pd
 from ta.trend import ema_indicator
 from ta.momentum import stochrsi_k, stochrsi_d
+import math
 from math import floor
 import random
 import datetime
@@ -13,10 +14,10 @@ import datetime
 # Variable values for testing
 
 # Interchange with any chart with the same column format
-chart = 'kucoin4h.csv'
+chart = 'charts/kucoin4h.csv'
 # chart = 'charts/largeDataSetETH.csv'
 # Starting capital
-initialCapital = 3000
+initialCapital = 1000
 # Set take profit (dollars)
 takeprofit = 15
 # Set stop loss (dollars)
@@ -28,7 +29,7 @@ cerebro = bt.Cerebro()
 feed = GenericCSVData(dataname=chart,
                       dtformat = ('%m/%d/%Y'),
                       # dtformat = ('%Y-%m-%d'),
-                      fromdate = datetime.datetime(2022, 10, 5),
+                      fromdate = datetime.datetime(2022, 1, 5),
                       date = 0,
                       open = 1,
                       high = 2,
@@ -85,19 +86,19 @@ class TestStrategy(bt.Strategy):
         price_current = pd.to_numeric(df['close'])[len(df) - 1]
         price_previous_open = pd.to_numeric(df_open['open'])[0 if len(df) == 1 else len(df) - 2]
         price_previous_low = pd.to_numeric(df_low['low'])[0 if len(df_low) == 1 else len(df_low) - 2]
-        ema_200_current = ema_indicator(pd.to_numeric(df['close']), 200, False)[0 if len(df) == 1 else len(df) - 2]
+        ema_200_current = ema_indicator(pd.to_numeric(df['close']), 200, False)[len(df) - 1]
         if(price_current < ema_200_current):
             return
         price_current_high = self.datahigh[0]
         price_current_open = self.dataopen[0]
         price_current_low = self.datalow[0]
         price_volume = self.datavolume[0]
-        sma_9_current = ema_indicator(pd.to_numeric(df_for_indicators['close']), 17, False)[0 if len(df_for_indicators) == 1 else len(df_for_indicators) - 2]
-        sma_9_trailing = ema_indicator(pd.to_numeric(df['close']), 17, False)[0 if ((len(df) == 1) or (len(df) == 2)) else len(df) - 3]
-        rsi_k_current = stochrsi_k(pd.to_numeric(df_for_indicators['close']), 14, 3, 3, True)[0 if len(df_for_indicators) == 1 else len(df_for_indicators) - 2]
-        rsi_d_current = stochrsi_d(pd.to_numeric(df_for_indicators['close']), 14, 3, 3, True)[0 if len(df_for_indicators) == 1 else len(df_for_indicators) - 2]
-        rsi_k_trailing = stochrsi_k(pd.to_numeric(df['close']), 14, 3, 3, True)[0 if ((len(df) == 1) or (len(df) == 2)) else len(df) - 3]
-        rsi_d_trailing = stochrsi_d(pd.to_numeric(df['close']), 14, 3, 3, True)[0 if ((len(df) == 1) or (len(df) == 2)) else len(df) - 3]
+        sma_9_current = ema_indicator(pd.to_numeric(df['close']), 17, False)[len(df) - 1]
+        sma_9_trailing = ema_indicator(pd.to_numeric(df['close']), 17, False)[0 if len(df) == 1 else len(df) - 2]
+        rsi_k_current = stochrsi_k(pd.to_numeric(df['close']), 14, 3, 3, True)[len(df) - 1]
+        rsi_d_current = stochrsi_d(pd.to_numeric(df['close']), 14, 3, 3, True)[len(df) - 1]
+        rsi_k_trailing = stochrsi_k(pd.to_numeric(df['close']), 14, 3, 3, True)[0 if len(df) == 1 else len(df) - 2]
+        rsi_d_trailing = stochrsi_d(pd.to_numeric(df['close']), 14, 3, 3, True)[0 if len(df) == 1 else len(df) - 2]
 
         # Buying conditions
         def kupward():
@@ -112,47 +113,45 @@ class TestStrategy(bt.Strategy):
 
         if(kupward() and smaupward() and priceup() and currentPosition.inPosition == False):
             currentPosition.inPosition = True
-            currentPosition.buyPrice = price_current_open
-            # if(int(price_current_open) > int(price_previous_open)):
-            #     currentPosition.buyPrice = random.randrange(int(price_previous_open), int(price_current_open))
-            # elif(int(price_current_open) < int(price_previous_open)):
-            #     currentPosition.buyPrice = random.randrange(int(price_current_open), int(price_previous_open))
-            # else:
-            #     currentPosition.buyPrice = (price_current_open + price_previous_open)/2
+            currentPosition.buyPrice = price_current
             positionStats.fees = positionStats.fees + ((positionStats.capital * leverage)/100)*0.06
             print('--------------------')
             self.log('BUY')
+            print('buy price: ')
             print(currentPosition.buyPrice)
-            print(price_current)
             print('--------------------')
             self.buy()
         
         lossStop = sma_9_current
         # Option to sell on buy tick
-        if(((rsi_k_current - rsi_k_trailing > 3) or (price_current < lossStop)) and currentPosition.inPosition == True):
+        if(((rsi_k_trailing - rsi_k_current > 0) or (price_current < lossStop)) and currentPosition.inPosition == True):
             self.log('SELL')
             # Win
-            if(rsi_k_current < rsi_k_trailing):
-                positionStats.wins += 1
+            if(rsi_k_trailing - rsi_k_current > 0):
+                if(currentPosition.buyPrice < price_current):
+                    positionStats.wins += 1
+                elif(currentPosition.buyPrice > price_current):
+                    positionStats.losses += 1
                 feeWin = ((((positionStats.capital * leverage))/100)*0.06) * 2
-                positionStats.capital = positionStats.capital + (((((positionStats.capital * leverage)/currentPosition.buyPrice)*(price_current)) - (positionStats.capital * leverage)) - feeWin)
-                positionStats.fees = positionStats.fees + (((positionStats.capital * leverage) + takeprofit)/100)*0.06
-                print(currentPosition.buyPrice + takeprofit)
+                print('profit: ')
+                print((((positionStats.capital * leverage)/currentPosition.buyPrice)*(price_current)) - ((((positionStats.capital * leverage)/currentPosition.buyPrice)*(currentPosition.buyPrice))) - feeWin)
+                positionStats.capital = positionStats.capital + ((((positionStats.capital * leverage)/currentPosition.buyPrice)*(price_current)) - ((((positionStats.capital * leverage)/currentPosition.buyPrice)*(currentPosition.buyPrice))) - feeWin)
+                print('capital:')
+                print(positionStats.capital)
+                print('sell price: ' )
+                print(price_current)
             # Loss
-            elif(price_current < lossStop):
+            elif(price_current < lossStop and False):
                 positionStats.losses += 1
                 # Fee calculation --> closing price / 100 * 0.06 (maker fee) * 2 (fee taken on buy and sell)
                 feeLoss = (((positionStats.capital * leverage)/100)*0.06) * 2
                 # Post trade capital calculation --> capital + ((capital * leverage) / buy price in ETH) * sell price in ETH - (initial capital * leverage) - fee for both buy/sell 
                 positionStats.capital = ((positionStats.capital + ((((positionStats.capital * leverage)/currentPosition.buyPrice)*(lossStop)) - (positionStats.capital * leverage))) - feeLoss)
-                # Add fees to running count
-                positionStats.fees = positionStats.fees + (lossStop/100)*0.06
-                print(lossStop)
+                print('sell price: ')
+                print(price_current)
 
-            print(price_current)
             currentPosition.inPosition = False
             currentPosition.buyPrice = 0
-            positionCooldown = True
             self.sell()
 
 cerebro.addstrategy(TestStrategy)
@@ -177,9 +176,6 @@ print(positionStats.losses)
 print('--------------------')
 print('Win rate:')
 print((positionStats.wins)/(positionStats.wins + positionStats.losses))
-print('--------------------')
-print('Fees:')
-print(positionStats.fees)
 print('--------------------')
 print('Final profit:')
 print(floor((positionStats.capital - positionStats.startingCapital)))
