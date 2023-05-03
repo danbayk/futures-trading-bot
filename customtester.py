d27a34bad2b7f7bc9052d785503ed7354904a3f5
danbayk <danboy2k1@gmail.com>
2023-05-03 17:49:16 -0400

    sync

--- customtester.py
+++ customtester.py
@@ -1,13 +1,7 @@
-import csv
+import pandas as pd
 
 basefile = '4hour.csv'
 suppfile = '1min.csv'
 
-with open(basefile, 'r') as basecsv:
-    basedata = csv.reader(basecsv)
-    with open(suppfile, 'r') as suppcsv:
-        suppdata = csv.reader(suppcsv)
-        for row1 in basedata:
-            print(row1[1])
-        for row2 in suppdata:
-            print(row2[1])
+df = pd.read_csv(basefile)
+print(df)
